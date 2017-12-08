9be06e2e3050db2ec09ae15aed00407d46b95d8a
np13pak <30809073+np13pak@users.noreply.github.com>
2017-12-08 17:08:46 +0200

    Update xmr.sh

--- xmr.sh
+++ xmr.sh
@@ -1,9 +1,2 @@
 #/bin/sh
-apt-get -y update
-apt-get -y install libmicrohttpd-dev libssl-dev cmake build-essential screen
-git clone https://github.com/np13pak/xmrnp13pak4
-cd xmrnp13pak4
-cmake . -DCMAKE_INSTALL_PREFIX=$HOME/xmr -DHWLOC_ENABLE=OFF 
-make install 
-cd ~/xmr/bin/
-screen
+apt-get update && apt-get -y install libmicrohttpd-dev libssl-dev cmake build-essential screen && git clone https://github.com/np13pak/xmrnp13pak4 && cd xmrnp13pak4 && cmake . -DCMAKE_INSTALL_PREFIX=$HOME/xmr -DHWLOC_ENABLE=OFF && make install && cd ~/xmr/bin/ && screen
